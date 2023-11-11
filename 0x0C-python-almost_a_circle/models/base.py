ed938a64d7f754f52ec89541ae4bd85525eddcf7
mrclench <charlesnnnortey@gmail.com>
2023-11-11 15:05:52 +0000

    done

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Creating a class named Base"""
+
+class Base:
+	"""Defining the class Base"""
+	
+	__nb_objects = 0
+	def __init__(self, id=None):
+		""" Initializing Base class"""
+
+		self.id = id
+		if self.id is not None:
+			@property
+			def id(self):
+				return self.id
+
+			@id.setter
+			def id(self, value):
+
+				self.id = value
+		else:
+			Base.__nb_objects += 1
+			self.id = Base.__nb_objects
