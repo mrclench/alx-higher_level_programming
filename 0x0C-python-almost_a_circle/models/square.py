415f24a94ab698679adf4d39ca6e82099b7693cd
mrclench <charlesnnnortey@gmail.com>
2023-11-14 23:05:05 +0000

    done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-"""Creating a new class called Square"""
+"""This module defines the Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle class"""
+    """
+    Square class that inherits from Rectangle class
+    Square class that inherits from Rectangle class.
+
+    Attributes:
+        size (int): The size of the square.
+        x (int): The x-coordinate of the square.
+        y (int): The y-coordinate of the square.
+        id (int): The identifier of the square.
+
+    Methods:
+        __init__(self, size, x=0, y=0, id=None): Initializes a new Square instance.
+        __str__(self): Returns a string representation of the square.
+    """
     def __init__(self, size, x=0, y=0, id=None):
         """Initializing the class Square"""
         super().__init__(size, size, x, y, id)
@@ -12,4 +25,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Printing string value of height and weight"""
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size))
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
