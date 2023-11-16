b28b5291094cbf5b5c310d112e592ba1a58271c9
mrclench <charlesnnnortey@gmail.com>
2023-11-16 10:05:29 -0800

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,10 @@
 """Creating a class named Base"""
 
 import json
+
 """imports json"""
+import csv
+import os
 
 
 class Base:
@@ -25,3 +28,21 @@ class Base:
                 not all(type(x) == dict for x in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+
+                Args:
+                    list_objs (list): list of instances who inherits of Base
+                """
+        if not isinstance(list_objs, list) and list_objs is not None:
+            raise TypeError("list_objs must be a list of instances")
+        if not all(isinstance(x, cls) for x in list_objs):
+            raise TypeError("list_objs must be a list of instances")
+
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+            if list_objs is not None:
+                list_dicts = [x.to_dictionary() for x in list_objs]
+                json.dump(list_dicts, f)
