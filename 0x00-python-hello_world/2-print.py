fac6a56c6ee751f33eaa33a2baa7a12ec966dada
mrclench <charlesnnnortey@gmail.com>
2024-02-19 05:17:59 +0000

    starting line

--- 0x00-python-hello_world/2-print.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-text = '"Programming is like building a multilingual puzzle'
-print(text)
--- 0x00-python-hello_world/4-print_float.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-number = 3.14159
-text = f"Float: {number:.2f}"
-print(text)
