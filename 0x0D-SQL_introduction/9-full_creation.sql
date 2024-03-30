7b3ae2f219ca6994ddde719e501e564a1661f042
mrclench <charlesnnnortey@gmail.com>
2024-03-30 15:52:09 -0700

    done

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,8 @@
 --  A script that creates a table named 'second_table' in the database hbtn_0c_0
+USE hbtn_0c_0;
+
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
-	score INT);
+	score INT
+	);
