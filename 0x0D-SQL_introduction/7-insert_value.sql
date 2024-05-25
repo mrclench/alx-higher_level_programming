4923b7de422a1077340e594797e565b5438ba04e
mrclench <charlesnnnortey@gmail.com>
2024-05-25 00:43:46 -0700

    done

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- this script that inserts a new row in the table named first_table
+-- this script that inserts a new row with values in  first_table
 INSERT INTO first_table(id, name) VALUES (89, 'Best School');
