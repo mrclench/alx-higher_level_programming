da668d7141e9fd279651135a2b56082f5deb0915
mrclench <charlesnnnortey@gmail.com>
2024-05-26 11:43:59 -0700

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- This script creates database hbtn_0d_2 and also creates a user 
+-- user_0d_2.
+-- Notes:
+-- Should only have the SELECT privilege in database hbtn_0d_2
+-- password for user will be user_0d_2_pwd and script should fail if any exists .
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
