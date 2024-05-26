8d656191e8192435d1b1d43c41d279b058f8553b
mrclench <charlesnnnortey@gmail.com>
2024-05-26 11:34:13 -0700

    done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,5 @@
 -- 	   password will be user_0d_1_pwd
 --	   script shouldnt fail if already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
