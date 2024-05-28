66154ddba86408d8c5b4e359b105c8e2f63b2cd5
mrclench <charlesnnnortey@gmail.com>
2024-05-27 17:34:44 -0700

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@
 -- use SELECT once
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.genre_id
-ORDER BY tv_shows.title;
+INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
