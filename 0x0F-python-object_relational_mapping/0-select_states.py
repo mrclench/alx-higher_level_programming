3729c3291d51675a45fe449c8145e7dab612b8f1
mrclench <charlesnnnortey@gmail.com>
2024-06-26 21:40:54 -0700

    indentation error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@ import sys
 if __name__ == "__main__":
     # Get data on states from database
     db = MySQLdb.connect(host="localhost",
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        port=3306)
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cur.fetchall()
@@ -20,4 +20,3 @@ for row in rows:
 cur.close()
 # Close all databases
 db.close()
-
