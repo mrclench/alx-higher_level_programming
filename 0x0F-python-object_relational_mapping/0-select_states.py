14859e3a6914598f92b5bd9b7d16c2fd68170db4
mrclench <charlesnnnortey@gmail.com>
2024-06-26 21:37:00 -0700

    First task done

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  lists all states from the new database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get data on states from database
+    db = MySQLdb.connect(host="localhost",
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        port=3306)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+# Close all cursors
+cur.close()
+# Close all databases
+db.close()
+
